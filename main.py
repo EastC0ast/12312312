5d17ae4895b6f3daadbd2ae47e225fa5c24b9a37
EastC0ast <110261391+EastC0ast@users.noreply.github.com>
2022-11-27 01:28:25 +0300

    Супер Крутая версия кликера почти работает

--- main.py
+++ main.py
@@ -14,18 +14,31 @@ if __name__ == '__main__':
     mont64 = pygame.font.Font(font, 64) # шрифт 64
     mont32 = pygame.font.Font(font, 32) # шрифт 32
     target_img = pygame.image.load('1st.png')
+    target2_img = pygame.image.load('2nd.png')
+    target3_img = pygame.image.load('3rd.png')
     background_image = pygame.image.load('background_img.png')
     lvl_img = pygame.image.load('lvl.png')
     lvl_img = pygame.transform.scale(lvl_img, (250, 283))
     lvl_img_rect = lvl_img.get_rect()
+    lvl2_img = pygame.image.load('1lvl.png')
+    lvl2_img = pygame.transform.scale(lvl2_img, (250, 283))
+    lvl2_img_rect = lvl_img.get_rect()
     running = True
     FPS = 60
-    score = 000000
+    level = 0
+    score = 0
 
     while running:
         screen.blit(background_image, (0, 0))
-        screen.blit(target_img, (80, 235))
-        screen.blit(lvl_img, (0, 40))
+        if level == 0:
+            screen.blit(lvl_img, (0, 40))
+            screen.blit(target_img, (80, 235))
+        elif level == 1:
+            screen.blit(lvl2_img, (0, 40))
+            screen.blit(target2_img, (80, 235))
+        elif level == 2:
+            screen.blit(lvl_img, (0, 40))
+            screen.blit(target3_img, (80, 235))
         text = mont64.render(f'Кол-во очков {score}', True, (255, 255, 255))
         place = text.get_rect(center = top_of_window)
         screen.blit(text, place)
@@ -35,14 +48,29 @@ if __name__ == '__main__':
                 exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if screen_rect.collidepoint(event.pos):
-                    score += 1
+                    if level == 0:
+                        score += 1
+                    elif level == 1:
+                        score += 2
+                    elif level == 2:
+                        score += 4
+                    elif level == 3:
+                        score += 8
+                    elif level == 4:
+                        score += 16
+                    elif level == 5:
+                        score += 32
                 if lvl_img_rect.collidepoint(event.pos):
-                    pass
-                    
-
-
+                    if score >= 100:
+                        if level == 0:
+                            level += 1
+                            score -= 100
+                        if score >= 500:
+                            if level == 1:
+                                level += 1
+                                score -= 500
+                            if score >= 1000:
+                                if level == 2:
+                                    level += 0
+                                    score -= 1000
         clock.tick(FPS)
-
-
-
-
