b92c759f7711269a40ceacb2ffec34b9a1fd0684
tdclmf <leelucoon@gmail.com>
2022-11-27 19:08:55 +0300

    add button, pygame-widget

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
+import pygame_widgets
+from pygame_widgets.button import Button
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('My Hommie Uses Clicker')
     clock = pygame.time.Clock()
     size = width, height = 650, 800
-    center_of_window = width // 2, height // 2 # центр экрана
-    top_of_window = width // 2, 35 # верх экрана
+    center_of_window = width // 2, height // 2  # центр экрана
+    top_of_window = width // 2, 35  # верх экрана
     bot_of_window = width // 2, 800  # верх экрана
-    screen = pygame.display.set_mode(size) # создание окна
+    screen = pygame.display.set_mode(size)  # создание окна
+    screen.fill((255, 255, 255))
+    score = 0
+    def but():
+        global ingame
+        ingame = False
+        print('Clicked!')
+    button = Button(
+        # Mandatory Parameters
+        screen,  # Surface to place button on
+        550,  # X-coordinate of top left corner
+        10,  # Y-coordinate of top left corner
+        90,  # Width
+        45,  # Height
+
+        text='Магазин',
+        fontSize=30,
+        margin=20,
+        radius=5,
+        onClick=lambda: but()
+    )
     screen_rect = screen.get_rect()
-    font = pygame.font.match_font('Mont') # шрифт
-    mont64 = pygame.font.Font(font, 64) # шрифт 64
-    mont32 = pygame.font.Font(font, 32) # шрифт 32
+    font = pygame.font.match_font('Mont')  # шрифт
+    mont64 = pygame.font.Font(font, 64)  # шрифт 64
+    mont32 = pygame.font.Font(font, 32)  # шрифт 32
     target_img = pygame.image.load('1st.png')
     target2_img = pygame.image.load('2nd.png')
     target3_img = pygame.image.load('3rd.png')
@@ -26,8 +48,7 @@ if __name__ == '__main__':
     running = True
     FPS = 60
     level = 0
-    score = 0
-
+    ingame = True
     while running:
         screen.blit(background_image, (0, 0))
         if level == 0:
@@ -40,13 +61,16 @@ if __name__ == '__main__':
             screen.blit(lvl_img, (0, 40))
             screen.blit(target3_img, (80, 235))
         text = mont64.render(f'Кол-во очков {score}', True, (255, 255, 255))
-        place = text.get_rect(center = top_of_window)
+        place = text.get_rect(center=top_of_window)
         screen.blit(text, place)
-        pygame.display.update()
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        pygame_widgets.update(events)
+        for event in events:
             if event.type == pygame.QUIT:
-                exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pygame.quit()
+                running = False
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and ingame:
                 if screen_rect.collidepoint(event.pos):
                     if level == 0:
                         score += 1
@@ -74,3 +98,4 @@ if __name__ == '__main__':
                                     level += 0
                                     score -= 1000
         clock.tick(FPS)
+        pygame.display.update()
