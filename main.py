d3e366a7b99480ce2c040dcad33623eb75305c02
tdclmf <leelucoon@gmail.com>
2022-11-27 19:48:43 +0300

    add button, pygame-widget

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ if __name__ == '__main__':
     pygame.display.set_caption('My Hommie Uses Clicker')
     clock = pygame.time.Clock()
     size = width, height = 650, 800
-    center_of_window = width // 2, height // 2  # центр экрана
-    top_of_window = width // 2, 35  # верх экрана
-    bot_of_window = width // 2, 800  # верх экрана
-    screen = pygame.display.set_mode(size)  # создание окна
+    center_of_window = width // 2, height // 2
+    top_of_window = width // 2, 35
+    bot_of_window = width // 2, 800
+    screen = pygame.display.set_mode(size)
     screen.fill((255, 255, 255))
     score = 0
+
+
     def but():
         global ingame
         ingame = False
         print('Clicked!')
+
+
     button = Button(
-        # Mandatory Parameters
-        screen,  # Surface to place button on
-        550,  # X-coordinate of top left corner
-        10,  # Y-coordinate of top left corner
-        90,  # Width
-        45,  # Height
+        screen,
+        550,
+        10,
+        90,
+        45,
 
         text='Магазин',
         fontSize=30,
@@ -32,19 +35,14 @@ if __name__ == '__main__':
         onClick=lambda: but()
     )
     screen_rect = screen.get_rect()
-    font = pygame.font.match_font('Mont')  # шрифт
-    mont64 = pygame.font.Font(font, 64)  # шрифт 64
-    mont32 = pygame.font.Font(font, 32)  # шрифт 32
-    target_img = pygame.image.load('1st.png')
-    target2_img = pygame.image.load('2nd.png')
-    target3_img = pygame.image.load('3rd.png')
+    font = pygame.font.match_font('Mont')
+    mont64, mont32 = pygame.font.Font(font, 64), pygame.font.Font(font, 32)
+    target_img, target2_img, target3_img = pygame.image.load('1st.png'), pygame.image.load(
+        '2nd.png'), pygame.image.load('3rd.png')
     background_image = pygame.image.load('background_img.png')
-    lvl_img = pygame.image.load('lvl.png')
-    lvl_img = pygame.transform.scale(lvl_img, (250, 283))
+    lvl_img, lvl2_img = pygame.image.load('lvl.png'), pygame.image.load('1lvl.png')
+    lvl_img, lvl2_img = pygame.transform.scale(lvl_img, (250, 283)), pygame.transform.scale(lvl2_img, (250, 283))
     lvl_img_rect = lvl_img.get_rect()
-    lvl2_img = pygame.image.load('1lvl.png')
-    lvl2_img = pygame.transform.scale(lvl2_img, (250, 283))
-    lvl2_img_rect = lvl_img.get_rect()
     running = True
     FPS = 60
     level = 0
