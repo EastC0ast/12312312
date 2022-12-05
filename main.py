1c297cc82783889633eb46ab444438c27e94fa19
tdclmf <leelucoon@gmail.com>
2022-12-05 20:42:03 +0300

    main menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame_widgets
+import pygame_menu
 from pygame_widgets.button import Button
 
 if __name__ == '__main__':
@@ -12,8 +13,6 @@ if __name__ == '__main__':
     bot_of_window = width // 2, 800
     screen = pygame.display.set_mode(size)
     screen.fill((255, 255, 255))
-    score = 0
-
 
     def but():
         global ingame
@@ -43,57 +42,67 @@ if __name__ == '__main__':
     lvl_img, lvl2_img = pygame.image.load('lvl.png'), pygame.image.load('1lvl.png')
     lvl_img, lvl2_img = pygame.transform.scale(lvl_img, (250, 283)), pygame.transform.scale(lvl2_img, (250, 283))
     lvl_img_rect = lvl_img.get_rect()
-    running = True
     FPS = 60
-    level = 0
     ingame = True
-    while running:
-        screen.blit(background_image, (0, 0))
-        if level == 0 or level == 2:
-            screen.blit(lvl_img, (0, 40))
-            if level == 0:
-                screen.blit(target_img, (80, 235))
-            else:
-                screen.blit(target3_img, (80, 235))
-        elif level == 1:
-            screen.blit(lvl2_img, (0, 40))
-            screen.blit(target2_img, (80, 235))
-        text = mont64.render(f'Кол-во очков {score}', True, (255, 255, 255))
-        place = text.get_rect(center=top_of_window)
-        screen.blit(text, place)
-        events = pygame.event.get()
-        pygame_widgets.update(events)
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                running = False
-                quit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and ingame:
-                if screen_rect.collidepoint(event.pos):
-                    if level == 0:
-                        score += 1
-                    elif level == 1:
-                        score += 2
-                    elif level == 2:
-                        score += 4
-                    elif level == 3:
-                        score += 8
-                    elif level == 4:
-                        score += 16
-                    elif level == 5:
-                        score += 32
-                if lvl_img_rect.collidepoint(event.pos):
-                    if score >= 100:
+
+    def game():
+        score = 0
+        level = 0
+        running = True
+        while running:
+            screen.blit(background_image, (0, 0))
+            if level == 0 or level == 2:
+                screen.blit(lvl_img, (0, 40))
+                if level == 0:
+                    screen.blit(target_img, (80, 235))
+                else:
+                    screen.blit(target3_img, (80, 235))
+            elif level == 1:
+                screen.blit(lvl2_img, (0, 40))
+                screen.blit(target2_img, (80, 235))
+            text = mont64.render(f'Кол-во очков {score}', True, (255, 255, 255))
+            place = text.get_rect(center=top_of_window)
+            screen.blit(text, place)
+            events = pygame.event.get()
+            pygame_widgets.update(events)
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    running = False
+                    quit()
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and ingame:
+                    if screen_rect.collidepoint(event.pos):
                         if level == 0:
-                            level += 1
-                            score -= 100
-                        if score >= 500:
-                            if level == 1:
+                            score += 1
+                        elif level == 1:
+                            score += 2
+                        elif level == 2:
+                            score += 4
+                        elif level == 3:
+                            score += 8
+                        elif level == 4:
+                            score += 16
+                        elif level == 5:
+                            score += 32
+                    if lvl_img_rect.collidepoint(event.pos):
+                        if score >= 100:
+                            if level == 0:
                                 level += 1
-                                score -= 500
-                            if score >= 1000:
-                                if level == 2:
-                                    level += 0
-                                    score -= 1000
-        clock.tick(FPS)
-        pygame.display.update()
+                                score -= 100
+                            if score >= 500:
+                                if level == 1:
+                                    level += 1
+                                    score -= 500
+                                if score >= 1000:
+                                    if level == 2:
+                                        level += 0
+                                        score -= 1000
+                pygame.display.update()
+
+
+    menu = pygame_menu.Menu('Welcome', 650, 800,
+                            theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.button('Play', game)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.mainloop(screen)
+
